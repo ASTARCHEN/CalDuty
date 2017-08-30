2afe0381a42522c67e82ecdb774e700ec014d72e
auhep <mathmanual@163.com>
2017-08-30 23:42:45 +0800

    要提交的变更： 	修改：     DutyData.py 	修改：     DutyMain.py

--- DutyData.py
+++ DutyData.py
@@ -7,73 +7,91 @@ import numpy as np
 # DutydFhsDecode: Decode fhs data
 # dRaw: fhs data with coding form
 # (return): fhs data with decoding form
-def DutyFhsDecode(dRaw):
+def DutyDataFhsDecode(dRaw):
     lenRaw = len(dRaw)
     for i in range(lenRaw):
         dRaw[i] = dRaw[i] - 2048
     return dRaw
 
-# DutyAccerationDecode: Decode acceration data
-# dRaw: acceration data with coding form
-# (return): acceration data with decoding form
-def DutyAccerationDecode(dRaw):
+# DutyAccerationDecode: Decode acceleration data
+# dRaw: acceleration data with coding form
+# (return): acceleration data with decoding form
+def DutyDataAccelerationDecode(dRaw):
     lenRaw = len(dRaw)
     for i in range(lenRaw):
         dRaw[i] = (dRaw[i] & 0x3FFF) - 2048
     return dRaw
     
 
-# DutyGetData: Obtain fhs(Fetal heart signal) and acceration signal from the file of fnRaw
+# DutyGetData: Obtain fhs(Fetal heart signal) and acceleration signal from the file of fnRaw
 # fnRaw: filename
 # dFhs: fhs signal, fs = 500Hz, 1 channel
-# dAcc: acceration signal, fs = 25Hz, 3 channels
-# cntLost: number of lost acceration packet
-def DutyGetData(fnRaw):
+# dAcc: acceleration signal, fs = 25Hz, 3 channels
+# cntLost: number of lost acceleration packet
+def DutyDataDecode(fnRaw):
     import os
     fExist = os.path.exists(fnRaw)
     dFhs = np.array([], dtype=np.short)
     dAcc = np.array([], dtype=np.short)    
     if(not fExist):
         return (dFhs, dAcc)
+    # The file exists, so decoding starts now.
+    fsAcc = 75
+    fsFhs = 500
+    dRaw = np.loadtxt(fnRaw, dtype=np.short)
+    lenRaw = len(dRaw)
+    # The first packet is a fhs packet or an acceleration packet, which is almost impossible to be complete.
+    # So, the first complete fhs packet should be identified before decoding. 
+    curStart = 0    # the first index of the current access data
+    if dRaw[0] < 10000:  #
+        tmpInd = 1
+        while dRaw[tmpInd] < 10000:
+            tmpInd += 1
+        #tmpInd -= 1
+        if tmpInd >= 500:
+            while tmpInd >= 500:
+                tmpInd -= 500
+            curStart = tmpInd
+        else:
+            curStart = tmpInd + 75
     else:
-        fsAcc = 75
-        fsFhs = 500
-        dRaw = np.loadtxt(fnRaw, dtype=np.short)
-        lenRaw = len(dRaw)
-        #print(lenRaw)
-        curStart = 0    # the first index of the current access data
-        while curStart < lenRaw:    # find the index of the first fhs data
-            if dRaw[curStart] < 10000:
+        tmpInd = 1
+        while dRaw[tmpInd] > 10000:
+            tmpInd += 1
+        curStart = tmpInd
+    # curStart has been set to point the first complete fhs packet.
+    #while curStart < lenRaw:    # find the index of the first fhs data
+     #   if dRaw[curStart] < 10000:
+      #      break
+      #  curStart += 1                
+    curEnd = 0      # the last index of the current access data
+    cntFhs = 0      # counter for fhs packets
+    cntAcc = 0      # counter for acceration packets
+    cntLost = 0     # counter for lost acceleration packets
+    while curStart <= lenRaw:
+        if dRaw[curStart] < 10000:  # fhs data
+            curEnd = curStart+fsFhs
+            if curEnd >= lenRaw:    # EXCEPTION: not a complete packet
                 break
-            curStart += 1                
-        curEnd = 0      # the last index of the current access data
-        cntFhs = 0      # counter for fhs data
-        cntAcc = 0      # counter for acceration data
-        cntLost = 0     # counter for lost acceration data
-        while curStart <= lenRaw:
-            if dRaw[curStart] < 10000:  # fhs data
-                curEnd = curStart+fsFhs
-                if curEnd >= lenRaw:    # EXCEPTION: not a complete packet
-                    break
-                cntFhs += 1
-                tmpFhs = DutyFhsDecode(dRaw[curStart:curEnd])
-                dFhs = np.hstack([dFhs, tmpFhs])
-                curStart = curEnd+1
-            else:                       # acceration data
-                curEnd = curStart+fsAcc
-                if curEnd >= lenRaw:    # EXCEPTION: not a complete packet
-                    break
-                cntAcc += 1
-                nLost = cntFhs - cntAcc
-                if nLost > 0:           # at least one acceration data packet lost 
-                    dAcc = np.hstack([dAcc, np.zeros(fsAcc*nLost, dtype=np.short)])     #fill with 0
-                    cntLost += nLost
-                tmpAcc = DutyAccerationDecode(dRaw[curStart:curEnd])
-                dAcc = np.hstack([dAcc, tmpAcc])
-                cntAcc = cntFhs
-                curStart = curEnd  
-        nLost = cntFhs - cntAcc     # duration of fhs data and that of acceration data should be the same 
-        if nLost > 0:               # at least one acceration data packet lost 
-            dAcc = np.hstack([dAcc, np.zeros(fsAcc*nLost, dtype=np.short)])
-            cntLost += nLost
+            cntFhs += 1
+            tmpFhs = DutyDataFhsDecode(dRaw[curStart:curEnd])
+            dFhs = np.hstack([dFhs, tmpFhs])
+            curStart = curEnd
+        else:                       # acceleration data
+            curEnd = curStart+fsAcc
+            if curEnd >= lenRaw:    # EXCEPTION: not a complete packet
+                break
+            cntAcc += 1
+            nLost = cntFhs - cntAcc
+            if nLost > 0:           # at least one acceleration data packet lost 
+                dAcc = np.hstack([dAcc, np.zeros(fsAcc*nLost, dtype=np.short)])     #fill with 0
+                cntLost += nLost
+            tmpAcc = DutyDataAccelerationDecode(dRaw[curStart:curEnd])
+            dAcc = np.hstack([dAcc, tmpAcc])
+            cntAcc = cntFhs
+            curStart = curEnd  
+    nLost = cntFhs - cntAcc     # duration of fhs data and that of acceleration data should be the same 
+    if nLost > 0:               # at least one acceleration data packet lost 
+        dAcc = np.hstack([dAcc, np.zeros(fsAcc*nLost, dtype=np.short)])
+        cntLost += nLost
     return (dFhs, dAcc, cntLost)
--- DutyMain.py
+++ DutyMain.py
@@ -1,19 +1,22 @@
 # This is a python prject, which is an attempt to acheive the functions of WallE
+# Development Enviroment: Python 3.5.3, GCC 6.3.0, Linux debian9 4.9.0-3-amd64
     
 import numpy as np
 import matplotlib.pyplot as plt
 from DutyData import *
-
+from DutyPlot import *
 
 if __name__ == '__main__':
 
     fnRaw = './y00004.txt'
-    (dFhs, dAcc, nLost) = DutyGetData(fnRaw)
+    (dFhs, dAcc, nLost) = DutyDataDecode(fnRaw)
     t = len(dFhs)/500
     if(t <= 0):
         print('Nonexistent or empty file.')
         SystemExit(0)
-    print('Accelation data has been seperated from fhs data.')
-    print('The test lasted %ds' %(t))
-    print('%d accelation data packets lost' %(nLost))
+    print('Acceleration data has been seperated from fhs data.')
+    print('The test was %ds in length' %(t))
+    print('%d acceleration packets lost' %(nLost))
+    DutyPlotFhs(dFhs)
+    #DutyPlotFhs(dAcc)
 
